7c288f66fe6755fe622ddea712018f3a882f08b0
Webs95 <alexandersemashko1995@yandex.ru>
2022-10-17 15:21:46 +0300

    feat: add improvments in solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,23 +2,30 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
 function createDreamTeam(members) {
   const firstLettersArr = [];
-  members.filter(el => typeof el === 'string').map(el => el.trim()).forEach(el => firstLettersArr.push(el[0]))
-  return firstLettersArr.sort().join('').toUpperCase();
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    members
+      .filter((el) => typeof el === 'string')
+      .map((el) => el.trim())
+      .forEach((el) => firstLettersArr.push(el[0]));
+    return firstLettersArr.sort().join('').toUpperCase();
+  }
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
